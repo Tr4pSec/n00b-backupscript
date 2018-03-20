cc3412a26b36004c7fa1ba9bab35c28e0636b36b
Max <maximilian.larum@gmail.com>
2018-03-19 19:47:55 -0500

    Update backupscript.ps1

--- backupscript.ps1
+++ backupscript.ps1
@@ -1,17 +1,8 @@
-﻿<#
-This script is supposed to copy a folder with subfolders and items to a "backup" folder on another drive. foldernames should include dates.
-If the destination dosent have more than 100GB free space, you should get a error.
-It also deletes any backups older than 15 days.
-
-This is intended to run as a scheduled task on a endpoint daily.
-
-Not fully tested, might produce unexpected results. Don't use this script if you can't afford to loose data.
-
-Written by @tr4psec
+<# Backup script by @Tr4pSec
+Copies one folder with subfolders to a destination drive. Creates a \backups\ folder, and a new folder named after the date when 
+the backup runs. It deletes backups older than 10 days. 
 #>
 
-
-#Error alert box
 Add-Type -AssemblyName PresentationCore,PresentationFramework
 $ButtonType = [System.Windows.MessageBoxButton]::OK
 $MessageIcon = [System.Windows.MessageBoxImage]::Error
@@ -19,31 +10,23 @@ $MessageBody = "Please check diskpace on target!"
 $MessageTitle = "Cannot perform backup"
 
 
-#Enter your source folder here (the folder you want to back up)
-$sourcefolder = "C:\backupthisfolder\documents\"
+$SourcePath = "F:\Mine dokumenter\Mine bilder\tegninglol" <#Enter the path to the folder you want to back up#>
+$DesinationDrive = "E:" <#Change this with the drive letter you want to use as the destination drive#>
 
-#Enter your destination Drive:
-$destinationdrive = "E:\"
 
-#Enter your destination path here (the folder you want the copy to end up in.)
-$destinationpath = "$desinationdrive\backups\$((Get-Date).ToString('yyyy-MM-dd'))"
+$DestinationPath = "$DesinationDrive\backups\$((Get-Date).ToString('yyyy-MM-dd'))" 
+$FreeSpace = Get-PSDrive | Where-Object {$_.Root -eq "$DesinationDrive\"} | Select-Object -ExpandProperty Free
+$FreeSpacetoGB = ($FreeSpace/1GB -as [int])
 
-$date = get-date
-$freespace = Get-PSDrive | Where-Object {$_.Root -eq "$destinationdrive"} | Select-Object -ExpandProperty Free
-$freespacetoGB = ("{0:N0}" -f ($freespace/1GB))
-
-#Only does action below if you have more than 100GB free space on destinationdrive
-if ($freespacetoGB -ge 100) {
-    New-Item -Type Directory -Path $destinationpath
-    Copy-Item $sourcefolder -Destination $destinationpath -Recurse -Verbose
-} else {
-
-$Result = [System.Windows.MessageBox]::Show($MessageBody,$MessageTitle,$ButtonType,$MessageIcon)
-
+if ($FreeSpacetoGB -ge 100) {
+    New-Item -Type Directory -Path $DestinationPath
+    Copy-Item $SourcePath -Destination $DestinationPath -Recurse -Verbose
+} else {    
+[System.Windows.MessageBox]::Show($MessageBody,$MessageTitle,$ButtonType,$MessageIcon)
 }
 
-#Deletes any folders older than $howold days
-$HowOld = -15 
-$Path = "$destinationdrive\backup"
-get-childitem $Path -recurse | where {$_.lastwritetime -lt (get-date).adddays($HowOld) -and -not $_.psiscontainer} |% {remove-item $_.fullname -force}
+
+$HowOld = -10 <# This number is how old (days) the folder needs to be before the script deletes it #>
+$Path = "$DesinationDrive\backups"
+get-childitem $Path -recurse | Where-Object {$_.lastwritetime -lt (get-date).adddays($HowOld)} | remove-item -Recurse -force -confirm:$false
 
