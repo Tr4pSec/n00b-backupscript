926bd3c6cbc9db22f6547727b4fdb3782dd593e3
Max <maximilian.larum@gmail.com>
2018-03-19 23:57:04 -0500

    Update backupscript.ps1

--- backupscript.ps1
+++ backupscript.ps1
@@ -2,13 +2,14 @@
 Copies one folder with subfolders to a destination drive. Creates a \backups\ folder, and a new folder named after the date when 
 the backup runs. It deletes backups older than 10 days. 
 #>
-
+function DiskSpaceAlert {
 Add-Type -AssemblyName PresentationCore,PresentationFramework
 $ButtonType = [System.Windows.MessageBoxButton]::OK
 $MessageIcon = [System.Windows.MessageBoxImage]::Error
 $MessageBody = "Please check diskpace on target!"
 $MessageTitle = "Cannot perform backup"
-
+[System.Windows.MessageBox]::Show($MessageBody,$MessageTitle,$ButtonType,$MessageIcon)
+}
 
 $SourcePath = "C:\folder\keep" <#Enter the path to the folder you want to back up#>
 $DesinationDrive = "E:" <#Change this with the drive letter you want to use as the destination drive#>
@@ -22,7 +23,7 @@ if ($FreeSpacetoGB -ge 100) {
     New-Item -Type Directory -Path $DestinationPath
     Copy-Item $SourcePath -Destination $DestinationPath -Recurse -Verbose
 } else {    
-[System.Windows.MessageBox]::Show($MessageBody,$MessageTitle,$ButtonType,$MessageIcon)
+DiskSpaceAlert
 }
 
 
